2bdb84fe54c50c96f8300e4a0c19e9d5ccb775c0
Charlie-007-star <sujinsr2457@gmail.com>
2021-11-14 00:13:25 +0530

    css studied new concept

--- first/src/components/Card.js
+++ first/src/components/Card.js
@@ -1,9 +1,13 @@
 import React , {useState} from "react";
+import heart from "../assets/heart-solid.svg";
 
 function Card(props) {
 
     let favorites = useState("Add to Fav"); 
+    let activeClass = useState("");
+
     let [btnTextState , setBtnTextState] = favorites;
+    let [classState, setClassState] = activeClass
 
     function addToFavorite() {
        
@@ -15,10 +19,20 @@ function Card(props) {
                return "Add to Fav"
            }
         })
+
+        setClassState((prevClass) => {
+
+            if(prevClass == ""){
+                return "active"
+            }else{
+                return "";
+            }
+        })
     }
 
     return (
-        <div className="card">
+        <div className={`card ${classState}`}>
+            <img src={heart} className="icon " />
             <img src={props.cardContent.imgSrc} alt=""/>
             <h2>{props.cardContent.name}</h2>
             <p>{props.cardContent.email}</p>
