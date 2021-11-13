a527684a3667953738702aec3f5022621418c38a
Charlie-007-star <sujinsr2457@gmail.com>
2021-11-13 23:40:00 +0530

    usestate learning

--- first/src/components/Card.js
+++ first/src/components/Card.js
@@ -1,13 +1,29 @@
-import React from "react";
-
+import React , {useState} from "react";
 
 function Card(props) {
+
+    let favorites = useState("Add to Fav"); 
+    let [btnTextState , setBtnTextState] = favorites;
+
+    function addToFavorite() {
+       
+        setBtnTextState((prevState) => {
+
+           if(prevState === "Add to Fav"){
+               return "Added"
+           }else{
+               return "Add to Fav"
+           }
+        })
+    }
 
     return (
         <div className="card">
             <img src={props.cardContent.imgSrc} alt=""/>
             <h2>{props.cardContent.name}</h2>
-            <p>Email: {props.cardContent.email}</p>
+            <p>{props.cardContent.email}</p>
+            <button className="button" onClick={addToFavorite}>{btnTextState}</button>
+            
         </div>
     );
 }
